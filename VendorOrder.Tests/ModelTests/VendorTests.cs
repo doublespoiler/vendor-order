5c4d1fef889b0e24bc693cb93a70179050deb2be
Skylan Lew <skylanslew@gmail.com>
2022-09-30 10:01:02 -0700

    Finish boilerplate

---
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorOrder.Models;
+using System.Collections.Generic;
+using System;
+
+namespace VendorOrder.Tests 
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Record.ClearAll();
+    }
+    [TestMethod]
+    public void CategoryConstructor_CreatesInstanceOfCategory_Category()
+    {
+
+    }
+
+  }
+}
---
+++ VendorOrder/Controllers/VendorController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace VendorOrder.Controllers
+{
+    public class VendorController : Controller
+    {
+
+    }
+}
