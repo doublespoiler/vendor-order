847a792fc6957c7eb2767b7841c246f674e0d3b8
Skylan Lew <skylanslew@gmail.com>
2022-09-30 22:19:25 -0700

    update styling

--- VendorOrder/wwwroot/js/scripts.js
+++ VendorOrder/wwwroot/js/scripts.js
@@ -1,18 +1,19 @@
-function myClock() {         
-  setTimeout(function() {   
+function myClock() {
+  setTimeout(function() {
     const d = new Date();
     const n = d.toDateString() + " " + d.toLocaleTimeString();
     document.getElementById("clock").innerHTML = n; 
-    myClock();             
+    myClock();
   }, 1000)
 }
 
 function randomBg(){
   const images = ['../images/bread1.jpg', '../images/bread2.jpg', '../images/bread3.jpg', '../images/bread4.jpg'];
   const body = document.getElementById("bodycontent");
-  body.style.backgroundImage = 'url(' + images[Math.floor(Math.random() * images.length)] + ')';
+  const url = 'url(' + images[Math.floor(Math.random() * images.length)] + ')';
+  body.style.background = 'linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),' + url;
+  body.style.backgroundSize= 'cover';
 }
 
- 
-window.addEventListener("DOMContentLoaded", myClock());
-window.addEventListener("DOMContentLoaded", randomBg());
+randomBg();
+myClock();
